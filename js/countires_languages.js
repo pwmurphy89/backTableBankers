cac0aa33de712d0c494fed8461318436d833a789
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-11 15:38:25 -0400

    added counties list  for languages

---
+++ js/countires_languages.js
@@ -0,0 +1,302 @@
+var china= {
+		name: "China",
+		languages: {
+
+		}
+	}	
+var india= {
+		name: "India",
+		languages: {
+
+		}
+	}	
+var unitedStates= {
+		name: "United States",
+		languages: {
+			
+		}
+	}	
+var indonesia= {
+		name: "Indonesia",
+		languages: {
+
+		}
+	}	
+var brazil= {
+		name: "Brazil",
+		languages: {
+
+		}
+	}	
+var pakistan= {
+		name: "Pakistan",
+		languages: {
+
+		}
+	}	
+var nigeria= {
+		name: "Nigeria",
+		languages: {
+
+		}
+	}	
+var bangladesh= {
+		name: "Bangladesh",
+		languages: {
+
+		}
+	}	
+var russia= {
+		name: "Russia",
+		languages: {
+
+		}
+	}	
+var japan= {
+		name: "Japan",
+		languages: {
+
+		}
+	}	
+var mexico= {
+		name: "Mexico",
+		languages: {
+
+		}
+	}	
+var philippines= {
+		name: "Philippines",
+		languages: {
+
+		}
+	}	
+var ethiopia= {
+		name: "Ethiopia",
+		languages: {
+
+		}
+	}	
+var vietnam= {
+		name: "Vietnam",
+		languages: {
+
+		}
+	}	
+var egypt= {
+		name: "Egypt",
+		languages: {
+
+		}
+	}	
+var iran= {
+		name: "Iran",
+		languages: {
+
+		}
+	}	
+var germany= {
+		name: "Germany",
+		languages: {
+
+		}
+	}	
+var turkey= {
+		name: "Turkey",
+		languages: {
+
+		}
+	}	
+var congoDeRev= {
+		name: "Congo De Rev",
+		languages: {
+
+		}
+	}	
+var thailand= {
+		name: "Thailand",
+		languages: {
+
+		}
+	}	
+var france= {
+		name: "France",
+		languages: {
+
+		}
+	}	
+var unitedKingdom= {
+		name: "United Kingdom",
+		languages: {
+
+		}
+	}	
+var italy= {
+		name: "Italy",
+		languages: {
+
+		}
+	}	
+var burma= {
+		name: "Burma",
+		languages: {
+
+		}
+	}	
+var southAfrica= {
+		name: "South Africa",
+		languages: {
+
+		}
+	}	
+var tanzania= {
+		name: "Tanzania",
+		languages: {
+
+		}
+	}	
+var southKorea= {
+		name: "Korea, South",
+		languages: {
+
+		}
+	}	
+var spain= {
+		name: "Spain",
+		languages: {
+
+		}
+	}	
+var colombia= {
+		name: "Colombia",
+		languages: {
+
+		}
+	}	
+var kenya= {
+		name: "Kenya",
+		languages: {
+
+		}
+	}	
+var ukraine= {
+		name: "Ukraine",
+		languages: {
+
+		}
+	}	
+var argentina= {
+		name: "Argentina",
+		languages: {
+
+		}
+	}	
+var algeria= {
+		name: "Algeria",
+		languages: {
+
+		}
+	}	
+var poland= {
+		name: "Poland",
+		languages: {
+
+		}
+	}	
+var uganda= {
+		name: "Uganda",
+		languages: {
+
+		}
+	}	
+var iraq= {
+		name: "Iraq",
+		languages: {
+
+		}
+	}	
+var sudan= {
+		name: "Sudan",
+		languages: {
+
+		}
+	}	
+var canada= {
+		name: "Canada",
+		languages: {
+
+		}
+	}	
+var morocco= {
+		name: "Morocco",
+		languages: {
+
+		}
+	}	
+var afghanistan= {
+		name: "Afghanistan",
+		languages: {
+
+		}
+	}	
+var nepal= {
+		name: "Nepal",
+		languages: {
+
+		}
+	}	
+var malaysia= {
+		name: "Malaysia",
+		languages: {
+
+		}
+	}	
+var peru= {
+		name: "Peru",
+		languages: {
+
+		}
+	}	
+var venezuela= {
+		name: "Venezuela",
+		languages: {
+
+		}
+	}	
+var uzbekistan= {
+		name: "Uzbekistan",
+		languages: {
+
+		}
+	}	
+var saudiArabia= {
+		name: "Saudi Arabia",
+		languages: {
+
+		}
+	}	
+var yemen= {
+		name: "Yemen",
+		languages: {
+
+		}
+	}	
+var ghana= {
+		name: "Ghana",
+		languages: {
+
+		}
+	}	
+var mozambiqu= {
+		name: "Mozambique",
+		languages: {
+
+		}
+	}	
+var northKorea= {
+		name: "Korea, North",
+		languages: {
+
+		}
+	}	
+
+
