fe3851acccd2708ad426c3334c0fbebe05407eb7
Jonathan Melin <melin.jonathan@gmail.com>
2016-04-11 17:52:08 -0400

    Moved script data from index.html to filterController.js

--- js/filterController.js
+++ js/filterController.js
@@ -1,5 +1,7 @@
 interactiveMap.controller('filterController', function($scope, $http, $routeParams){
 
+	var map = new Datamap({element: document.getElementById('map')});
+
 	$scope.languages = languages;
 
 	// findLanguage()
