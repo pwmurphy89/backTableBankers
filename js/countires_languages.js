282533b778c13221f8be03e9abedc0685cc87258
pwmurphy89 <pwmurphy89@gmail.com>
2016-04-11 16:18:53 -0400

    Added languages to countries_languages.js

--- js/countires_languages.js
+++ js/countires_languages.js
@@ -1,301 +1,345 @@
 var china= {
 		name: "China",
 		languages: {
+			Mandarin,
+			Cantonese
 
 		}
 	}	
 var india= {
 		name: "India",
 		languages: {
-
+				Hindi,
+				English
 		}
 	}	
 var unitedStates= {
 		name: "United States",
 		languages: {
+			English,
+			Spanish,
+			Chinese
 			
 		}
 	}	
 var indonesia= {
 		name: "Indonesia",
 		languages: {
+			Bahasa Indonesia,
+			English,
+			Javanese 
 
 		}
 	}	
 var brazil= {
 		name: "Brazil",
 		languages: {
-
+				Portuguese,
+				Spanish,
+				English,
+				French
 		}
 	}	
 var pakistan= {
 		name: "Pakistan",
 		languages: {
+				Urdu,
+				English,
+				Punjabi
 
 		}
 	}	
 var nigeria= {
 		name: "Nigeria",
 		languages: {
-
+				English,
+				French
 		}
 	}	
 var bangladesh= {
 		name: "Bangladesh",
 		languages: {
-
+			Bengali,
+			English
 		}
 	}	
 var russia= {
 		name: "Russia",
 		languages: {
-
+					Russian, 
 		}
 	}	
 var japan= {
 		name: "Japan",
 		languages: {
+				Japanese
 
 		}
 	}	
 var mexico= {
 		name: "Mexico",
 		languages: {
-
+				Spanish 
 		}
 	}	
 var philippines= {
 		name: "Philippines",
 		languages: {
-
+			Filipino,
+			English
 		}
 	}	
 var ethiopia= {
 		name: "Ethiopia",
 		languages: {
-
+			Oromiffa,
+			English
 		}
 	}	
 var vietnam= {
 		name: "Vietnam",
 		languages: {
-
+			Vietnamese,
+			French
 		}
 	}	
 var egypt= {
 		name: "Egypt",
 		languages: {
-
+			Arabic,
+			English
 		}
 	}	
 var iran= {
 		name: "Iran",
 		languages: {
-
+			Persian
 		}
 	}	
 var germany= {
 		name: "Germany",
 		languages: {
-
+			German,
+			French,
+			English
 		}
 	}	
 var turkey= {
 		name: "Turkey",
 		languages: {
-
+			Turkish
 		}
 	}	
 var congoDeRev= {
 		name: "Congo De Rev",
 		languages: {
-
+			French
 		}
 	}	
 var thailand= {
 		name: "Thailand",
 		languages: {
-
+			Thai
 		}
 	}	
 var france= {
 		name: "France",
 		languages: {
-
+			French
 		}
 	}	
 var unitedKingdom= {
 		name: "United Kingdom",
 		languages: {
-
+			English
 		}
 	}	
 var italy= {
 		name: "Italy",
 		languages: {
-
+			Italian
 		}
 	}	
 var burma= {
 		name: "Burma",
 		languages: {
-
+			Burmese,
+			Chinese,
+			English
 		}
 	}	
 var southAfrica= {
 		name: "South Africa",
 		languages: {
-
+			Afrikaans,
+			English
 		}
 	}	
 var tanzania= {
 		name: "Tanzania",
 		languages: {
-
+			Swahili,
+			English
 		}
 	}	
 var southKorea= {
 		name: "Korea, South",
 		languages: {
-
+			Korean,
+			English
 		}
 	}	
 var spain= {
 		name: "Spain",
 		languages: {
-
+			Spanish,
+			Catalan
 		}
 	}	
 var colombia= {
 		name: "Colombia",
 		languages: {
-
+			Spanish
 		}
 	}	
 var kenya= {
 		name: "Kenya",
 		languages: {
-
+			English,
+			Swahili
 		}
 	}	
 var ukraine= {
 		name: "Ukraine",
 		languages: {
-
+			Ukrainian,
+			Russian
 		}
 	}	
 var argentina= {
 		name: "Argentina",
 		languages: {
-
+			Spanish
 		}
 	}	
 var algeria= {
 		name: "Algeria",
 		languages: {
-
+			Arabic,
+			Berber,
+			French
 		}
 	}	
 var poland= {
 		name: "Poland",
 		languages: {
-
+			Polish
 		}
 	}	
 var uganda= {
 		name: "Uganda",
 		languages: {
-
+			Swahili,
+			English
 		}
 	}	
 var iraq= {
 		name: "Iraq",
 		languages: {
-
+			Arabic,
+			Kurdish
 		}
 	}	
 var sudan= {
 		name: "Sudan",
 		languages: {
-
+			Arabic,
+			English
 		}
 	}	
 var canada= {
 		name: "Canada",
 		languages: {
-
+			French,
+			English
 		}
 	}	
 var morocco= {
 		name: "Morocco",
 		languages: {
-
+			Arabic,
+			Berber,
+			French
 		}
 	}	
 var afghanistan= {
 		name: "Afghanistan",
 		languages: {
-
+			Pashto,
+			Dari
 		}
 	}	
 var nepal= {
 		name: "Nepal",
 		languages: {
-
+			Nepali
 		}
 	}	
 var malaysia= {
 		name: "Malaysia",
 		languages: {
-
+			Malay,
+			English,
+			Chinese
 		}
 	}	
 var peru= {
 		name: "Peru",
 		languages: {
-
+			Spanish
 		}
 	}	
 var venezuela= {
 		name: "Venezuela",
 		languages: {
-
+			Spanish
 		}
 	}	
 var uzbekistan= {
 		name: "Uzbekistan",
 		languages: {
-
+			Uzbek,
+			Russian
 		}
 	}	
 var saudiArabia= {
 		name: "Saudi Arabia",
 		languages: {
-
+			Arabic
 		}
 	}	
 var yemen= {
 		name: "Yemen",
 		languages: {
-
+			Arabic
 		}
 	}	
 var ghana= {
 		name: "Ghana",
 		languages: {
-
+			English
 		}
 	}	
 var mozambiqu= {
 		name: "Mozambique",
 		languages: {
-
+			Portuguese
 		}
 	}	
 var northKorea= {
 		name: "Korea, North",
 		languages: {
-
+			Korean
 		}
 	}	
 
