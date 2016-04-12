dcb7cae418312c5047c41fad36e0ae7b0f4ece00
Jonathan Melin <melin.jonathan@gmail.com>
2016-04-12 10:17:48 -0400

    Added color and key to map

--- js/filterController.js
+++ js/filterController.js
@@ -1,7 +1,32 @@
 
 interactiveMap.controller('filterController', function($scope, $http, $routeParams){
 
-	var map = new Datamap({element: document.getElementById('map')});
+	var map = new Datamap({
+		element: document.getElementById('map'),
+		fills: {
+			HIGH: '#b80000',
+			LOW: '#ff8080',
+			MEDIUM: '#d93f3f',
+			UNKNOWN: 'rgb(0,0,0)',
+			defaultFill: '#ccc'
+		},
+		data: {
+			ESP: {
+				fillKey: 'HIGH'
+			},
+			MEX: {
+				fillKey: 'HIGH'
+			},
+			USA: {
+				fillKey: 'MEDIUM'
+			},
+			GBR: {
+				fillKey: 'LOW'
+			}
+		}
+	});
+
+	map.legend();
 
 	$scope.languages = languages;
 
