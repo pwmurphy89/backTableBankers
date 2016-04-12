a70925055666c89cb2d3432238930beae6ac1272
pwmurphy89 <pwmurphy89@gmail.com>
2016-04-12 14:55:59 -0400

    Linking FillKeys with countryCodes

--- js/filterController.js
+++ js/filterController.js
@@ -1,53 +1,49 @@
 
 interactiveMap.controller('filterController', function($scope, $http, $routeParams){
-
-	var map = new Datamap({
+	$scope.languages = languages;
+	$scope.countries = countries;
+	var data = {};
+	$scope.map = new Datamap({
 		element: document.getElementById('map'),
 		fills: {
-			HIGH: '#b80000',
-			LOW: '#ff8080',
-			MEDIUM: '#d93f3f',
-			UNKNOWN: 'rgb(0,0,0)',
-			defaultFill: '#ccc'
-		},
-		data: {
-			ESP: {
-				fillKey: 'HIGH'
-			},
-			MEX: {
-				fillKey: 'HIGH'
-			},
-			USA: {
-				fillKey: 'MEDIUM'
-			},
-			GBR: {
-				fillKey: 'LOW'
-			}
+			defaultFill: '#CCC'
 		}
 	});
 
-	map.legend();
-
-	$scope.languages = languages;
-	console.log($scope.languages);
-	$scope.countries = countries;
-
 	$scope.findLanguage =function(){
 
-		// Lists countries with selected language
-		for(var i = 0; i < countries.length;i++){
-					var x = Object.getOwnPropertyNames(countries[i].languages);
-					if(x.indexOf($scope.selectedLanguage) > -1){
-						// Gets Value of selected language in countries that have it
-						console.log(countries[i]);
-				    	var langPerc = countries[i].languages[$scope.selectedLanguage];
-				    	console.log(langPerc);
-				    	//if langPerc > 20{
-				    		//do this
-				    	
+		var map = new Datamap({
+			element: document.getElementById('map'),
+			fills: {
+				HIGH: '#b80000',
+				LOW: '#ff8080',
+				MEDIUM: '#d93f3f',
+				UNKNOWN: 'rgb(0,0,0)',
+				defaultFill: '#ccc'
+			},
+			data
+		});
 
-		}
-	}
+		// Lists countries with selected language
+	for(var i = 0; i < countries.length;i++){
+		var x = Object.getOwnPropertyNames(countries[i].languages);
+			if(x.indexOf($scope.selectedLanguage) > -1){
+				// Gets Value of selected language in countries that have it
+				console.log(countries[i]);
+		    	var langPerc = countries[i].languages[$scope.selectedLanguage];
+		    	console.log(langPerc);
+
+		    		if (langPerc < 33){
+		    			data[countries[i].countryCode] = {fillKey: "LOW"};
+			    	}else if(langPerc<66){
+			    		data[countries[i].countryCode] = {fillKey: "MEDIUM"};
+			    	}else {
+			    		data[countries[i].countryCode] = {fillKey: "HIGH"};
+			    	}
+					    	
+				}
+			}
+	console.log(data)
 }
 
 
@@ -56,7 +52,7 @@ interactiveMap.controller('filterController', function($scope, $http, $routePara
 
 	// findGdp()
 
-
+	// map.legend();
 
 
 }); 
