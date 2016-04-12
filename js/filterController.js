6867d77dd69ecd5d1d457a7c32f1a93fc15fa632
pwmurphy89 <pwmurphy89@gmail.com>
2016-04-12 15:46:33 -0400

    added updateChoropleth function and removed second instance of the map.  Allow fill keys to change

--- js/filterController.js
+++ js/filterController.js
@@ -6,53 +6,36 @@ interactiveMap.controller('filterController', function($scope, $http, $routePara
 	$scope.map = new Datamap({
 		element: document.getElementById('map'),
 		fills: {
-			defaultFill: '#CCC'
-		}
+			HIGH: '#b80000',
+			LOW: '#ff8080',
+			MEDIUM: '#d93f3f',
+			UNKNOWN: 'rgb(0,0,0)',
+			defaultFill: '#ccc'
+			}
+
 	});
 
 	$scope.findLanguage =function(){
-
-		var map = new Datamap({
-			element: document.getElementById('map'),
-			fills: {
-				HIGH: '#b80000',
-				LOW: '#ff8080',
-				MEDIUM: '#d93f3f',
-				UNKNOWN: 'rgb(0,0,0)',
-				defaultFill: '#ccc'
-			},
-			data
-		});
-
 		// Lists countries with selected language
 	for(var i = 0; i < countries.length;i++){
+		data[countries[i].countryCode] = {fillKey: "defaultFill"};
 		var x = Object.getOwnPropertyNames(countries[i].languages);
 			if(x.indexOf($scope.selectedLanguage) > -1){
 				// Gets Value of selected language in countries that have it
-				console.log(countries[i]);
 		    	var langPerc = countries[i].languages[$scope.selectedLanguage];
-		    	console.log(langPerc);
-
 		    		if (langPerc < 33){
 		    			data[countries[i].countryCode] = {fillKey: "LOW"};
 			    	}else if(langPerc<66){
 			    		data[countries[i].countryCode] = {fillKey: "MEDIUM"};
 			    	}else {
 			    		data[countries[i].countryCode] = {fillKey: "HIGH"};
-			    	}
-					    	
+			    	}	    	
 				}
 			}
 	console.log(data)
+	$scope.map.updateChoropleth(data);
 }
-
-
-
 	// findPopulation()
-
 	// findGdp()
-
-	// map.legend();
-
-
+	$scope.map.legend();
 }); 
