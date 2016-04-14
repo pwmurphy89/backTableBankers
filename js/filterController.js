b76ef7b67887862e582dc5e3cdccce60680f460b
Keith Moore <keith.m4@gmail.com>
2016-04-14 17:50:29 -0400

    integrated dev branch filter with patrick's $scope.on function (changes dropdowns according to routeParams)

--- js/filterController.js
+++ js/filterController.js
@@ -6,73 +6,58 @@ interactiveMap.controller('filterController', function($scope, $http, $routePara
 
 	$scope.$on('$routeChangeSuccess', function() {
 
-		var optionType1 = returnOptionType($routeParams.option1);
-		var optionType2 = returnOptionType($routeParams.option2);
-		var optionType3 = returnOptionType($routeParams.option3);
+		$scope.selectedLanguage = $routeParams.option1;
 
-		if(optionType1 == 'language'){
-			$scope.selectedLanguage = $routeParams.option1;
-		}else if(optionType1 == 'population'){
-			$scope.selectedPopulation = $routeParams.option1;
-			for(var i = 0; i < popRanges.length;i++){
-				if (popRanges[i].id == $routeParams.option1){
-					$scope.selectedPopulation = popRanges[i];
-				}
-			}
-		}else{
-			for(var i = 0; i < gdpRanges.length;i++){
-				if (gdpRanges[i].id == $routeParams.option1){
-					$scope.selectedGDP = gdpRanges[i];
-				}
+		for(var i = 0; i < popRanges.length;i++){
+			if (popRanges[i].id == $routeParams.option2){
+				$scope.selectedPopulation = popRanges[i];
 			}
 		}
 
-		if(optionType2 == 'language'){
-			$scope.selectedLanguage = $routeParams.option2;
-		}else if(optionType2 == 'population'){
-			$scope.selectedPopulation = $routeParams.option2;
-			for(var i = 0; i < popRanges.length;i++){
-				if (popRanges[i].id == $routeParams.option2){
-					$scope.selectedPopulation = popRanges[i];
-				}
+		for(var i = 0; i < gdpRanges.length;i++){
+			if (gdpRanges[i].id == $routeParams.option3){
+				$scope.selectedGDP = gdpRanges[i];
 			}
-		}else{
-			for(var i = 0; i < gdpRanges.length;i++){
-				if (gdpRanges[i].id == $routeParams.option2){
-					$scope.selectedGDP = gdpRanges[i];
-				}
-			}
-		}
-
-		if(optionType3 == 'language'){
-			$scope.selectedLanguage = $routeParams.option3;
-		}else if(optionType3 == 'population'){
-			$scope.selectedPopulation = $routeParams.option3;
-			for(var i = 0; i < popRanges.length;i++){
-				if (popRanges[i].id == $routeParams.option3){
-					$scope.selectedPopulation = popRanges[i];
-				}
-			}
-		}else{
-			for(var i = 0; i < gdpRanges.length;i++){
-				if (gdpRanges[i].id == $routeParams.option3){
-					$scope.selectedGDP = gdpRanges[i];
-				}
-			}
-		}		
+		}	
   	});
 
-	$scope.doFilter = function(){
-		var optionArray = [];
-		if($scope.selectedLanguage){
-			optionArray.push($scope.selectedLanguage);
+	$scope.doFilter = function(filterType){
+		var newString = '';
+		var option1Type = returnOptionType($routeParams.option1);
+		var option2Type = returnOptionType($routeParams.option2);
+		var option3Type = returnOptionType($routeParams.option3);
+
+		if (filterType == 'one'){
+			var newOptionType = returnOptionType($scope.selectedLanguage);
+		} else if (filterType == 'two') {
+			var newOptionType = returnOptionType($scope.selectedPopulation.id);
+		} else if (filterType == 'three') {
+			var newOptionType = returnOptionType($scope.selectedGDP.id);
 		}
-		if($scope.selectedPopulation){
-			optionArray.push($scope.selectedPopulation.id);
+		var newOption = '';
+		console.log(newOptionType);
+		
+		if (newOptionType == 'language'){
+			newOption = $scope.selectedLanguage;
+			if (option2Type == '' && option3Type == ''){
+				newString = newOption;
+			} else if (option3Type == ''){
+				newString = newOption + '/' + $routeParams.option2;
+			} else {
+				newString = newOption + '/' + $routeParams.option2 + '/' + $routeParams.option3;
+			}
+		} else if (newOptionType == 'population'){
+			newOption = $scope.selectedPopulation.id;
+			if (option3Type == ''){
+				newString = $routeParams.option1 + '/' + newOption;
+			} else {
+				newString = $routeParams.option1 + '/' + newOption + '/' + $routeParams.option3;
+			}
+		} else if (newOptionType == 'gdp'){
+			newOption = $scope.selectedGDP.id;
+			newString = $routeParams.option1 + '/' + $routeParams.option2 + '/' + newOption;
 		}
-		if($scope.selectedGDP){
-			optionArray.push($scope.selectedGDP.id);
-		}
-		$location.path(optionArray.join('/'));  
+		
+		$location.path(newString);
 	}
 });
