2d4db872e115469b61a08fb2ab9acb9b1b0cc0b9
Jonathan Melin <melin.jonathan@gmail.com>
2016-04-14 18:54:38 -0400

    Styling changes and custom popups

--- js/homeMapController.js
+++ js/homeMapController.js
@@ -5,7 +5,20 @@ interactiveMap.controller('homeMapController', function($scope, $http, $routePar
 		fills: {
 			defaultFill: '#ccc'
 		},
-		responsive: true
+		responsive: true,
+		geographyConfig: {
+			popupTemplate: function(geo) {
+				var countryName = geo.properties.name;
+				for(i=0;i<countries.length;i++){
+					if (countryName === countries[i].name){
+						return '<div class="hoverinfo"><strong>' + countryName + '</strong>' +
+						'<p>Languages: ' + countries[i].languages + '</p>' +
+						'<p>Population: ' + countries[i].totalPop + '</p>' +
+						'<p>GDP: ' + countries[i].gdp + '</p></div>';
+					}
+				}			
+			}
+		}
 	});
 
 	window.addEventListener('resize', function() {
