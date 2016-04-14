02cfaa49a0bb5ce8f484a68c6448430cc24440d8
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-14 12:56:53 -0400

    Created combine functions to merge data objects.

--- js/mapsController.js
+++ js/mapsController.js
@@ -20,74 +20,144 @@ interactiveMap.controller('mapsController', function($scope, $http, $routeParams
 
 
 
+<<<<<<< Updated upstream
 	data = {};
 
 	if (language !== ''){
 
+=======
+	drawLanguage();
+	drawPopulation();
+	drawGDP();
+
+
+	if (language !== '' && population !== '' && gdp !== ''){
+		masterData = combine3(langData, popData, gdpData);
+		console.log(masterData);
+		drawMap(masterData);
+	}
+	else if (language !== '' && population !== ''){
+		masterData = combine2(langData, popData);
+		drawMap(masterData);
+	}
+	else if (population  !== '' && gdp !== ''){
+		masterData = combine2(popData, gdpData);
+		drawMap(masterData);
+	}
+	else if (language !== '' && gdp !== ''){
+		masterData = combine2(langData, gdpData);
+		drawMap(masterData);
+	}
+	else if (language !== ''){
+		drawMap(langData);
+	}
+	else if (population !== ''){
+		drawMap(popData);
+	}
+	else if (gdp !== ''){
+		drawMap(gdpData);
+	}
+
+
+	function combine2(primaryObj, secondaryObj){
+		for(var key in primaryObj){
+			if(secondaryObj.hasOwnProperty(key)){
+			}else{
+				delete primaryObj[key];
+			}			
+		}
+		return primaryObj;
+	}
+
+	function combine3(primaryObj, secondaryOneObj, secondaryTwoObj){
+		for(var key in primaryObj){
+			if(secondaryOneObj.hasOwnProperty(key) && secondaryTwoObj.hasOwnProperty(key)){
+			}else{
+				delete primaryObj[key];
+			}	
+		}
+		return primaryObj;
+	}
+
+	function drawLanguage(){	
+		console.log("Calculating Languages");
+		langData = {};	
+			// Lists countries with selected language
+>>>>>>> Stashed changes
 		for(var i = 0; i < countries.length;i++){
 			var x = Object.getOwnPropertyNames(countries[i].languages);
 			if(x.indexOf(language) > -1){
 				// Gets Value of selected language in countries that have it
 		    	var langPerc = countries[i].languages[language];
 	    		if(langPerc < 20){
-	    			data[countries[i].countryCode] = {fillKey: "LOW"};
+	    			langData[countries[i].countryCode] = {fillKey: "LOW"};
 		    	}else if(langPerc<40){
-		    		data[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
+		    		langData[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
 		    	}else if(langPerc<60){
-		    		data[countries[i].countryCode] = {fillKey: "MEDIUM"};
+		    		langData[countries[i].countryCode] = {fillKey: "MEDIUM"};
 		    	}else if(langPerc<80){
-		    		data[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
+		    		langData[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
 		    	}else{
-		    		data[countries[i].countryCode] = {fillKey: "HIGH"};
+		    		langData[countries[i].countryCode] = {fillKey: "HIGH"};
 		    	};		    	
 			};
+<<<<<<< Updated upstream
 		};
 	};
 
 	if (population !== ''){
 
+=======
+		};	
+	}
+
+	function drawPopulation(){
+		console.log("Calculating Population");
+		popData = {};
+>>>>>>> Stashed changes
 		for(var i = 0; i < countries.length; i++){
-			data[countries[i].countryCode] = {fillKey: "defaultFill"};
+			popData[countries[i].countryCode] = {fillKey: "defaultFill"};
 			var myPop = countries[i].totalPop;
 			// need to add filter based on routeParams value (myPopId)	
 
 			if (myPop < popRanges[0].value){
-				data[countries[i].countryCode] = {fillKey: "LOW"};
+				popData[countries[i].countryCode] = {fillKey: "LOW"};
 			}else if(myPop < popRanges[1].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
+				popData[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
 			}else if(myPop < popRanges[2].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM"};
+				popData[countries[i].countryCode] = {fillKey: "MEDIUM"};
 			}else if(myPop < popRanges[3].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
+				popData[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
 			}else {
-				data[countries[i].countryCode] = {fillKey: "HIGH"};
+				popData[countries[i].countryCode] = {fillKey: "HIGH"};
 			}    	
 		}	
 
 		if (population === 1){
-			for (var key in data){
-				if (data[key].fillKey !== "LOW"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in popData){
+				if (popData[key].fillKey !== "LOW"){
+					delete popData[key];
 				}
 			}
 		}else if (population === 2){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM-LOW"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in popData){
+				if (popData[key].fillKey !== "MEDIUM-LOW"){
+					delete popData[key];
 				}
 			}
 		}else if (population === 3){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in popData){
+				if (popData[key].fillKey !== "MEDIUM"){
+					delete popData[key];
 				}
 			}
 		}else if (population === 4){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM-HIGH"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in popData){
+				if (popData[key].fillKey !== "MEDIUM-HIGH"){
+					delete popData[key];
 				}
 			}
+<<<<<<< Updated upstream
 		}else{
 			for (var key in data){
 				if (data[key].fillKey !== "HIGH"){
@@ -99,47 +169,62 @@ interactiveMap.controller('mapsController', function($scope, $http, $routeParams
 
 	if (gdp !== ''){
 
+=======
+		}else
+			for (var key in popData){
+				if (popData[key].fillKey !== "HIGH"){
+					delete popData[key];
+				}
+			}
+	}
+
+	function drawGDP(){
+		console.log("Calculating GDP");
+		gdpData = {};
+		// also more needs to be done to get this working
+>>>>>>> Stashed changes
 		for(var i = 0; i < countries.length; i++){
-			data[countries[i].countryCode] = {fillKey: "defaultFill"};
+			gdpData[countries[i].countryCode] = {fillKey: "defaultFill"};
 			var thisGDP = countries[i].gdp;
 			// need to add filter based on routeParams value (gdp)
 			if (thisGDP < gdpRanges[0].value){
-				data[countries[i].countryCode] = {fillKey: "LOW"};
+				gdpData[countries[i].countryCode] = {fillKey: "LOW"};
 			}else if(thisGDP < gdpRanges[1].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
+				gdpData[countries[i].countryCode] = {fillKey: "MEDIUM-LOW"};
 			}else if(thisGDP < gdpRanges[2].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM"};
+				gdpData[countries[i].countryCode] = {fillKey: "MEDIUM"};
 			}else if(thisGDP < gdpRanges[3].value){
-				data[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
+				gdpData[countries[i].countryCode] = {fillKey: "MEDIUM-HIGH"};
 			}else{
-				data[countries[i].countryCode] = {fillKey: "HIGH"};
+				gdpData[countries[i].countryCode] = {fillKey: "HIGH"};
 			}	
 		}	
 
 		if (gdp === 1){
-			for (var key in data){
-				if (data[key].fillKey !== "LOW"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in gdpData){
+				if (gdpData[key].fillKey !== "LOW"){
+					delete gdpData[key];
 				}
 			}
 		}else if (gdp === 2){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM-LOW"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in gdpData){
+				if (gdpData[key].fillKey !== "MEDIUM-LOW"){
+					delete gdpData[key];
 				}
 			}
 		}else if (gdp === 3){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in gdpData){
+				if (gdpData[key].fillKey !== "MEDIUM"){
+					delete gdpData[key];
 				}
 			}
 		}else if (gdp === 4){
-			for (var key in data){
-				if (data[key].fillKey !== "MEDIUM-HIGH"){
-					data[key].fillKey = {fillKey: "defaultFill"};
+			for (var key in gdpData){
+				if (gdpData[key].fillKey !== "MEDIUM-HIGH"){
+					delete gdpData[key];
 				}
 			}
+<<<<<<< Updated upstream
 		}else{
 			for (var key in data){
 				if (data[key].fillKey !== "HIGH"){
@@ -529,6 +614,32 @@ if (language !== '' && population !== '' && gdp !== ''){
 // 			}
 
 	
+=======
+		}else
+			for (var key in gdpData){
+				if (gdpData[key].fillKey !== "HIGH"){
+					delete gdpData[key];
+				}
+			}	
+	}
+
+function drawMap(data){
+	map = new Datamap({
+			element: document.getElementById('map'),
+			fills: {
+				'HIGH': '#7d0000',
+				'MEDIUM-HIGH': '#a61e1e',
+				'MEDIUM': '#D95757',
+				'MEDIUM-LOW': '#FF9C9C',
+				'LOW': '#FFD6D6',
+				defaultFill: '#ccc'
+			},
+			data
+		});	
+
+		map.legend();
+}
+>>>>>>> Stashed changes
 
 // 		var map = new Datamap({
 // 			element: document.getElementById('map'),
