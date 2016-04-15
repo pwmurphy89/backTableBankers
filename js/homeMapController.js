969fa07184c55ce88b70f0fe45f3c2dc7c8acf84
Jonathan Melin <melin.jonathan@gmail.com>
2016-04-14 21:24:50 -0400

    Continued work on popups.

--- js/homeMapController.js
+++ js/homeMapController.js
@@ -8,13 +8,25 @@ interactiveMap.controller('homeMapController', function($scope, $http, $routePar
 		responsive: true,
 		geographyConfig: {
 			popupTemplate: function(geo) {
+				
 				var countryName = geo.properties.name;
 				for(i=0;i<countries.length;i++){
+
+					var languageObject = countries[i].languages;
+					var languageText = '';
+					for (property in languageObject){
+						languageText += property + '(' + languageObject[property] + '%)' + '; ';
+					};
+
+					var population = countries[i].totalPop;
+
+					var gdp = '$' + Math.round(countries[i].gdp);
+
 					if (countryName === countries[i].name){
 						return '<div class="hoverinfo"><strong>' + countryName + '</strong>' +
-						'<p>Languages: ' + countries[i].languages + '</p>' +
-						'<p>Population: ' + countries[i].totalPop + '</p>' +
-						'<p>GDP: ' + countries[i].gdp + '</p></div>';
+						'<p>Languages: ' + languageText + '</p>' +
+						'<p>Population: ' + population + '</p>' +
+						'<p>GDP: ' + gdp + '</p></div>';
 					}
 				}			
 			}
